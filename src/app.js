31db0ed9f7d94afb9434b958e1178bbf2da80057
Franco Correa <franco@francocorrea.com.uy>
2017-11-26 23:32:42 -0300

    New web design

--- src/app.js
+++ src/app.js
@@ -1,24 +1,51 @@
 require('dotenv');
 
 import Hapi from 'hapi';
+import Path from 'path';
 
-const server = new Hapi.Server();
+const server = new Hapi.Server({
+  connections: {
+    routes: {
+      files: {
+        relativeTo: Path.join(__dirname, 'static')
+      }
+    }
+  }
+});
 
 server.connection({
   host: process.env.HOST || '0.0.0.0',
   port: process.env.PORT || 5000
 });
 
-const plugins = [];
+const plugins = [require('vision'), require('inert')];
 
 server.register(plugins, err => {
   if (err) throw err;
+
+  server.views({
+    engines: { pug: require('pug') },
+    path: __dirname + '/templates',
+    compileOptions: { pretty: process.env.NODE_ENV !== 'production' }
+  });
 
   server.route({
     method: 'GET',
     path: '/',
     handler: (req, res) => {
-      res.redirect('https://www.twitter.com/francorreasosa?utm_source=fccomuy');
+      res.view('index');
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/static/{param*}',
+    handler: {
+      directory: {
+        path: '.',
+        redirectToSlash: true,
+        index: true
+      }
     }
   });
 
